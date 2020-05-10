efb84489cae968e375d7e8b7a7fa0b36332a38de
Tanish Chauhan <Tanish.Chauhan@iiitb.org>
2020-05-10 16:18:47 +0530

    Basic menu created

---
+++ src/main/java/com/iiitb/spe/calc/Calculator.java
@@ -0,0 +1,17 @@
+package com.iiitb.spe.calc;
+import java.util.*;
+
+public class Calculator {
+public static void main(String args[]) {
+	System.out.println("Welcome to Basic Calculator System");
+	System.out.println("");
+	System.out.println("Option Menu for Basic Operations");
+    System.out.println("");
+    System.out.println("1) Addition");
+    System.out.println("2) Subtraction");
+    System.out.println("3) Division");
+    System.out.println("4) Multiplication");
+    System.out.println("5) Exit");
+}
+
+}

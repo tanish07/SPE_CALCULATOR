561c191968480ec2c8f6800fb1ba86a0352fb071
Tanish Chauhan <Tanish.Chauhan@iiitb.org>
2020-05-10 16:48:58 +0530

    Added addition operation

--- src/main/java/com/iiitb/spe/calc/Calculator.java
+++ src/main/java/com/iiitb/spe/calc/Calculator.java
@@ -2,16 +2,43 @@ package com.iiitb.spe.calc;
 import java.util.*;
 
 public class Calculator {
-public static void main(String args[]) {
-	System.out.println("Welcome to Basic Calculator System");
-	System.out.println("");
-	System.out.println("Option Menu for Basic Operations");
-    System.out.println("");
-    System.out.println("1) Addition");
-    System.out.println("2) Subtraction");
-    System.out.println("3) Division");
-    System.out.println("4) Multiplication");
-    System.out.println("5) Exit");
-}
+	public static void main(String args[]) {
+		boolean execute = true;
+		int choice;
+		double number1, number2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to Basic Calculator System");
+		System.out.println("");
+		do {
+			System.out.println("Option Menu for Basic Operations");
+			System.out.println("");
+			System.out.println("1) Addition");
+			System.out.println("2) Subtraction");
+			System.out.println("3) Division");
+			System.out.println("4) Multiplication");
+			System.out.println("5) Exit");
+			choice = sc.nextInt();
+			if (choice == 5) {
+				execute = false;
+			} else {
+				switch (choice) {
+				case 1:
+					System.out.println("Addition");
+					System.out.println("Enter the two numbers");
+					System.out.println("Enter number 1");
+					number1 = sc.nextDouble();
+					System.out.println("Enter number 2");
+					number2 = sc.nextDouble();
+					System.out.println(number1 + number2);
+					System.out.println("");
+					break;
+				default:
+					System.out.println("Exiting program due to invalid input");
+					execute = false;
+
+				}
+			}
+		} while (execute == true);
+	}
 
 }
